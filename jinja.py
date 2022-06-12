2b1944f60f7071f080c7ce44ceed6bb51d35dd8b
Sergey Ryapin <rs1180@mail.ru>
2022-06-12 17:37:35 +0500

    Added into macros

--- jinja.py
+++ jinja.py
@@ -1,5 +1,6 @@
 from jinja2 import Template
 
+# фильтры
 # cars = [
 #     {'model': 'Ауди', 'price': 23000},
 #     {'model': 'Шкода', 'price': 17300},
@@ -13,19 +14,66 @@ from jinja2 import Template
 #
 # print(msg)
 
+# persons = [
+#     {'name': 'алексей', 'old': 18, 'weight': 78.5},
+#     {'name': 'николай', 'old': 28, 'weight': 82.3},
+#     {'name': 'иван', 'old': 33, 'weight': 94.0}
+# ]
+#
+# tpl = """
+# {% for u in users -%}
+# {% filter capitalize %}{{u.name}}{% endfilter %}
+# {% endfor -%}
+# """
+#
+# tm = Template(tpl)
+# msg = tm.render(users=persons)
+#
+# print(msg)
+
+# Макроопределения
+# html = """
+# {% macro input(name, value="None", type="text", size=20) -%}
+#     <input type="{{ type }}" name="{{name}}" value="{{ value|e }}" size="{{ size }}">
+# {%- endmacro %}}
+#
+# <p>{{ input('username', "Hello") }}
+# <p>{{ input('email') }}
+# <p>{{ input('password') }}
+# """
+#
+# tm = Template(html)
+# msg = tm.render()
+#
+# print(msg)
+
+# Вложенные макросы
+
 persons = [
-    {'name': 'Алексей', 'old': 18, 'weight': 78.5},
-    {'name': 'Николай', 'old': 28, 'weight': 82.3},
-    {'name': 'Иван', 'old': 33, 'weight': 94.0}
+    {'name': 'алексей', 'old': 18, 'weight': 78.5},
+    {'name': 'николай', 'old': 28, 'weight': 82.3},
+    {'name': 'иван', 'old': 33, 'weight': 94.0}
 ]
 
-tpl = """
-{% for u in users -%}
-{% filter upper %}{{u.name}}{% endfilter %}
-{% endfor -%}
+html = """
+{% macro list_users(list_of_users) -%}
+<ul>
+{% for u in list_of_users -%}
+    <li>{% filter upper %}{{u.name}}{% endfilter %} {{caller(u)}}
+{%- endfor -%}
+</ul>
+{%- endmacro -%}
+
+{% call(user) list_users(users) %}
+    <ul>
+    <li>age: {{user.old}}
+    <li>weight: {{user.weight}}
+    </ul>
+{% endcall %}
+
 """
 
-tm = Template(tpl)
+tm = Template(html)
 msg = tm.render(users=persons)
 
 print(msg)
